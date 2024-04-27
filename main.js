787414dfdf35ed7a03c1a14822548f5646161457
maryamsartaj <168002522+maryamsartaj@users.noreply.github.com>
2024-04-28 00:33:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var members = ["kohinoor", "anila", "narmeen", "lareeb", "sana"];
+var message = "kal birthday ha kis ki:";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ main.ts
@@ -0,0 +1,5 @@
+let members: string[] =[`kohinoor`,`anila`,`narmeen`,`lareeb`,`sana`];
+let message: string=`kal birthday ha kis ki:`;
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
